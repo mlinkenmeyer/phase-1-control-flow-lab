7e0be91eb6f8d1e3f3fa8c157b21d85845675248
Madison <mlinkenmeyer@gmail.com>
2023-07-31 15:23:12 -0700

    control flow lab complete

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance) {
+  let scuberGreetingForFeet;
+
+  if (rideDistance <= 400) {
+    scuberGreetingForFeet = "This one is on me!";
+  } else if (rideDistance <= 2000) {
+    scuberGreetingForFeet = "That will be twenty bucks.";
+  } else if (rideDistance <= 2500) {
+    scuberGreetingForFeet = "I will gladly take your thirty bucks.";
+  } else {
+    scuberGreetingForFeet = "No can do.";
+  }
+
+  return scuberGreetingForFeet;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(cityName) {
+  let cityAccess = cityName === "NYC" ? "Ok, sounds good." : "No go.";
+
+  return cityAccess;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount) {
+  let tipMessage;
+
+  switch (tipAmount) {
+    case "generous":
+      tipMessage = "Thank you so much.";
+      break;
+    case "not as generous":
+      tipMessage = "Thank you.";
+      break;
+    default:
+      tipMessage = "Bye.";
+      break;
+  }
+
+  return tipMessage;
 }
